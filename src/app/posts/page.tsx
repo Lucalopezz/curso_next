c150c3e22e85c67e8c5cfa5f2b506b6184947ec8
Lucas <lucaslops12@gmail.com>
2025-02-05 21:33:07 -0300

    client and server components

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,12 +1,32 @@
-
-
-//Server Components
-const Posts = () => {
-  return (
-    <div>
-      Poste
-    </div>
-  )
+interface PostProps {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
 }
 
-export default Posts
+interface RespProps {
+  posts: PostProps[];
+}
+
+//Server Components
+const Posts = async () => {
+  const resp = await fetch("https://dummyjson.com/posts");
+  const data: RespProps = await resp.json();
+
+  return (
+    <div>
+      <h1 className="text-center mt-5 mb-2 text-3xl">Posts</h1>
+      <div className="flex flex-col gap-4 mx-2">
+        {data.posts.map((post) => (
+          <div key={post.id} className="bg-zinc-800 p-4 rounded-md">
+            <h2 className="font-bold">{post.title}</h2>
+            <p>{post.body}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Posts;
